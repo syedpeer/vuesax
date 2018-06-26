52f24849224ebba4d7e050ef4a76ae66b4cc29b6
Luis Daniel Rovira <luisdanielrovira8@gmail.com>
2018-06-25 21:49:20 -0400

    deploy

---
+++ assets/js/47.9326284c.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[47],{450:function(n,t,e){},564:function(n,t,e){"use strict";var s=e(450);e.n(s).a},749:function(n,t,e){"use strict";e.r(t);var s={},i=(e(564),e(0)),o=Object(i.a)(s,function(){var n=this.$createElement;return(this._self._c||n)("div",{},[this._v("\n  hola soy el demo\n")])},[],!1,null,null,null);t.default=o.exports}}]);
